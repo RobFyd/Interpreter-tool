af9599ffbf393a3850942470e6f5dba5dd7d23b4
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2025-01-16 00:28:21 +0000

    add loading state to Panel component for improved user experience

--- front-end/src/components/Panel/Panel.jsx
+++ front-end/src/components/Panel/Panel.jsx
@@ -4,13 +4,20 @@ import styles from "./Panel.module.css";
 
 export function Panel() {
   const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
     fetch("http://localhost:3000/words")
       .then((response) => response.json())
       .then((response) => {
         setData(response);
+        setIsLoading(false);
       });
   }, []);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
 
   return (
     <>
